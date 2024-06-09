06f491d803d3bd672caff22afa66ed6d9ff2ce64
Bryan Suárez <bryan@taxo.co>
2024-06-09 18:41:18 -0500

    add a create task dto and refactoring

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreatetaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -13,10 +14,7 @@ export class TasksController {
 
   // create a task
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Task {
-    return this.tasksService.createTask(title, description);
+  createTask(@Body() createtaskDto: CreatetaskDto): Task {
+    return this.tasksService.createTask(createtaskDto);
   }
 }
